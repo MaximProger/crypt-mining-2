59f653302b67c2b56a9a2401788ab642beb20405
Maksim Koshman <maksimkoshman090@gmail.com>
2021-03-11 06:40:00 +0300

    Поменял инпут на селект

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,66 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Поиск родителя
+  function findAncestor(el, cls) {
+    while ((el = el.parentElement) && !el.classList.contains(cls));
+    return el;
+  }
+
+  function closeSelects() {
+    const selects = document.querySelectorAll(".exchange__select");
+    selects.forEach((select) => {
+      select.classList.remove("exchange__select--active");
+    });
+  }
+
+  const selectsHead = document.querySelectorAll(".exchange__select__head");
+  if (selectsHead) {
+    selectsHead.forEach((selectHead) => {
+      selectHead.addEventListener("click", () => {
+        const currSelect = selectHead.parentNode;
+
+        if (currSelect.classList.contains("exchange__select--active")) {
+          currSelect.classList.remove("exchange__select--active");
+        } else {
+          closeSelects();
+          currSelect.classList.add("exchange__select--active");
+        }
+      });
+    });
+  }
+
+  if (selectsHead) {
+    document.onclick = function (evt) {
+      if (
+        evt.target.className != "exchange__select" &&
+        !findAncestor(evt.target, "exchange__select")
+      ) {
+        closeSelects();
+      }
+    };
+  }
+
+  const selectItems = document.querySelectorAll(".exchange__select__item");
+  if (selectItems) {
+    selectItems.forEach((selectItem) => {
+      selectItem.addEventListener("click", () => {
+        const currSelect = findAncestor(selectItem, "exchange__select");
+        const currSelectHead = currSelect.querySelector(
+          ".exchange__select__head"
+        );
+        currSelectHead.innerHTML = selectItem.innerHTML;
+        currSelect.classList.remove("exchange__select--active");
+      });
+    });
+  }
+
+  const openSelect = document.querySelector("#openSelect");
+  if (openSelect) {
+    openSelect.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      openSelect.parentNode.classList.toggle("exchange__inner__choose--active");
+    });
+  }
+
+  // Tooltip
   tippy("[data-tippy-content]");
 });
